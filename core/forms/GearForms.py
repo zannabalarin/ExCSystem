73347359a120b9f8555d5270edd15dce49b27568
Tomek <tomaszmf95@yahoo.com>
2018-02-14 21:59:43 -0800

    Seperated GearCreate into GearCreateBefin and GearFinish

--- core/forms/GearForms.py
+++ core/forms/GearForms.py
@@ -2,9 +2,11 @@ from django import forms
 from core.models import Gear
 
 
-class GearCreationForm(forms.ModelForm):
+class GearBeginCreateForm(forms.ModelForm):
     """
     The basic form to create a piece of Gear. Mostly follows django admin defaults
+
+    This creates a generic piece of gear and based on that information redirects to a more specific gear creation form
 
     This uses the ModelForm (like the rest of django's admin) to automatically translate the model into a Form, View and
     the related HTML. Therefore, what is contained here simply overrides some default functionality, and explicit views
@@ -13,9 +15,21 @@ class GearCreationForm(forms.ModelForm):
 
     class Meta:
         model = Gear
+        fields = ("department", "rfid")
 
 
-class GearRetagForm(forms.ModelForm):
+class GearFinishForm(forms.ModelForm):
+    """
+    Form to complete filling out data the data required for a specific type of gear
+
+    This form is redirected to based on the choices made in the GearBeginCreateForm. It should adjust the data it
+    requires based on the exact type of gear that was chosen
+    """
+
+    pass
+
+
+class GearReTagForm(forms.ModelForm):
     """
     Form to expedite the process of re-taging (changing the RFID) a piece of gear
 
@@ -26,3 +40,4 @@ class GearRetagForm(forms.ModelForm):
 
     class Meta:
         model = Gear
+        fields = ("rfid", )
