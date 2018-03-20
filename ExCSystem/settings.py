3bcfd6410751138ad0785f82b6022b3f8d20e507
Håvard Anda Estensen <haavard.ae@gmail.com>
2018-03-20 12:06:18 -0700

    Add empty staticfiles folder

--- ExCSystem/settings.py
+++ ExCSystem/settings.py
@@ -127,6 +127,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
 
